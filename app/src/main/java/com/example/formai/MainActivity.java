ad3e04ca2632c7f3937a9632c151470de5572457
s7560m <haydenhoffman@gmail.com>
2021-01-16 15:29:05 -0500

    Camera preview works

--- app/src/main/java/com/example/formai/MainActivity.java
+++ app/src/main/java/com/example/formai/MainActivity.java
@@ -1,14 +1,145 @@
 package com.example.formai;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.camera2.Camera2Config;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.CameraXConfig;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.ProgressBar;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.pose.Pose;
+import com.google.mlkit.vision.pose.PoseLandmark;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Define cameraprovider and previewview fields
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    // Get the image capture
+    private ImageCapture imageCapture;
+
+    // Get the progress bar
+    private ProgressBar progressBar;
+
+    // Get the pose from onCreate()
+    private String poseFromIntent;
+
+    // Get floating action button
+    private FloatingActionButton cameraButton;
+
+    // Method to bind camera to preview view
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider, PreviewView previewView) {
+        Size resolution = new Size(360, 480);
+        // Generate preview with target resolution
+        Preview preview = new Preview.Builder()
+                .setTargetResolution(resolution)
+                .build();
+
+        // Build the camera and select the back lens
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build();
+
+        // Generate the image analysis
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(resolution)
+                // This sets how it will handle the data pipeline.
+                // Pipeline handler to deal with keeping only latest image. Drop other image frames
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+        // Get main executor thread to feed into analyzer
+        imageAnalysis.setAnalyzer(getMainExecutor(), imageProxy -> {
+            // Grab image from phone's camera
+            @SuppressLint("UnsafeExperimentalUsageError")
+            Image mediaImage = imageProxy.getImage();
+            if (mediaImage != null) {
+
+                // Feed image pipeline into pose classifier API
+                InputImage image = InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+            }
+
+        });
+
+
+        // Set up image capture methods
+        this.imageCapture =
+                new ImageCapture.Builder()
+                        .build();
+
+        // Create the lifecycle, bind camera preview to previewView
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageCapture, imageAnalysis, preview);
+        preview.setSurfaceProvider(previewView.createSurfaceProvider());
+
+    }
+
+    public CameraXConfig getCameraXConfig() {
+        return Camera2Config.defaultConfig();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the initial prompt
+        new MaterialAlertDialogBuilder(this)
+                .setTitle("Start posing!")
+                .setMessage("Position your camera so it can show your full body. When you've held that position correctly for a few seconds, you'll receive a checkmark for that pose!")
+                .setPositiveButton("Accept", (dialog, which) -> {
+                    dialog.dismiss();
+                })
+                .show();
+        // Define the processCameraProvider
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        // When the camera provider resolves, it will bind the hardware camera to the preview
+        cameraProviderFuture.addListener(() -> {
+            try {
+                // If the listener resolves, define the cameraProvider and bind the cameraProvider to the previewView
+                PreviewView previewView = (PreviewView) findViewById(R.id.previewView);
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider, previewView);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(this));
     }
+
+
 }
